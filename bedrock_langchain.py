6232a795d789637959f38ed28cd85112e158f3b0
ly2xxx <ly2xxx@hotmail.com>
2024-06-08 15:42:11 +0100

    fix Bedrock warning

--- bedrock_langchain.py
+++ bedrock_langchain.py
@@ -1,13 +1,15 @@
 
-from langchain_community.llms import Bedrock
+# from langchain_community.llms import Bedrock
+from langchain_aws import BedrockLLM
 import dotenv
 
 #load environment variables from .env file
 dotenv.load_dotenv()
 
-llm = Bedrock(model_id="anthropic.claude-v2")
+# llm = Bedrock(model_id="anthropic.claude-v2")
+llm = BedrockLLM(model_id="anthropic.claude-v2")
 
-prompt = "What is the largest city in Vermont?"
+prompt = "What is the largest city in Scotland?"
 
 response_text = llm.invoke(prompt) #return a response to the prompt
 
