a73b021bbc14ec3eb81bd73d09cab9676cdd206d
ly2xxx <ly2xxx@hotmail.com>
2024-06-19 00:26:29 +0100

    Part 2 - langsmith

--- FINX_LANGGRAPH/setup_environment.py
+++ FINX_LANGGRAPH/setup_environment.py
@@ -1,7 +1,17 @@
 import os
+from datetime import date
 
 from decouple import config
 
 
-def set_environment_variables() -> None:
+def set_environment_variables(project_name: str = "") -> None:
+    if not project_name:
+        project_name = f"Test_{date.today()}"
+
     os.environ["OPENAI_API_KEY"] = str(config("OPENAI_API_KEY"))
+
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    os.environ["LANGCHAIN_API_KEY"] = str(config("LANGCHAIN_API_KEY"))
+    os.environ["LANGCHAIN_PROJECT"] = project_name
+
+    print("API Keys loaded and tracing set with project name: ", project_name)
